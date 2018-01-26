db9e09a34d422a14d02b8c8b4c4cd87502a48543
Ion Ioxa <ion77ioxa@gmail.com>
2018-01-25 21:06:52 -0500

    Initial Commit

---
+++ Chuck Norris Facts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chuck Norris Facts
+//
+//  Created by Ion Ioxa on 2018-01-25.
+//  Copyright © 2018 Ion Ioxa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
