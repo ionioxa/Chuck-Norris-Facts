4e744fa6c3edf3b19948fddb22c87c76e8414944
Ion Ioxa <ion77ioxa@gmail.com>
2018-01-29 21:11:00 -0500

    Add labels and images of the project and applied all the constrains to the view

--- Chuck Norris Facts/Controller/ViewController.swift
+++ Chuck Norris Facts/Controller/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var jokes: UILabel!
+    @IBOutlet weak var jokesNumber: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func shareBtn(_ sender: UIButton) {
     }
-
-
+    
+    @IBAction func favoriteBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func randomBtn(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func menuBtn(_ sender: UIButton) {
+    }
+    
+    
 }
 
