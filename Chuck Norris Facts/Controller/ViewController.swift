844b184917896df65576d914c63205482ae0bacb
Ion Ioxa <ion77ioxa@gmail.com>
2018-02-21 22:44:19 -0500

    Using Alamofire, initiating the networking function and parsing the JSON data into the labels

--- Chuck Norris Facts/Controller/ViewController.swift
+++ Chuck Norris Facts/Controller/ViewController.swift
@@ -7,10 +7,16 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SVProgressHUD
+
 
 class ViewController: UIViewController {
 
-    
+    let randomJokesURL = "http://api.icndb.com/jokes/random"
+    var joke : String = ""
+    var jokeId : Int = 0
     
     @IBOutlet weak var jokes: UILabel!
     @IBOutlet weak var jokesNumber: UILabel!
@@ -18,12 +24,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.getJokesData(url: randomJokesURL)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        //
-    }
 
     @IBAction func shareBtn(_ sender: UIButton) {
     }
@@ -32,12 +35,61 @@ class ViewController: UIViewController {
     }
     
     @IBAction func randomBtn(_ sender: UIButton) {
+        self.getJokesData(url: randomJokesURL)
     }
     
     
     @IBAction func menuBtn(_ sender: UIButton) {
     }
     
+    //MARK: - Networking
+    /************************************************************************/
+    func getJokesData(url: String) {
+        
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+                    print("Success! Got the jokes")
+                    let jokesJSON : JSON = JSON(response.result.value!)
+                    self.updateJokes(json: jokesJSON)
+                    print(jokesJSON)
+                    
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    self.jokes.text = "Connection Error"
+                }
+                
+        }
+        
+    }
+    
+    //MARK: - JSON Parsing
+    /************************************************************************/
+    
+    func updateJokes (json: JSON) {
+        
+        if let jokesResult = json["value"]["joke"].string {
+            
+            joke = String(jokesResult)
+            jokes.text = "\(joke)"
+            
+        } else {
+            
+            jokes.text = "Hmm...No Jokes Available"
+            
+        }
+        
+        if let jokesId = json["value"]["id"].int {
+            
+            jokeId = Int(jokesId)
+            jokesNumber.text = "#\(jokeId)"
+            
+        } else {
+            
+            jokesNumber.text = "..."
+            
+        }
+    }
     
 }
 
